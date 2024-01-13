e7e3a0456714b4e7d9b65cc951862a1a70e3b1d0
Bernatmorenobatlle <156532892+Bernatmorenobatlle@users.noreply.github.com>
2024-01-13 17:24:20 +0000

    final commit

--- Hello.py
+++ Hello.py
@@ -1,51 +1,244 @@
-# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# Importing necessary module for handling URL errors
+from urllib.error import URLError
 
+# Importing pandas for data manipulation and streamlit for building web applications
+import pandas as pd
 import streamlit as st
-from streamlit.logger import get_logger
 
-LOGGER = get_logger(__name__)
+# Setting page configuration for the Streamlit app (wide mode, window title, page title and subheader)
+st.set_page_config(page_title="TFM - Bernat Moreno Batlle", layout="wide")
+st.markdown("# DISEÑO E IMPLEMENTACIÓN DE UN PIPELINE BIOINFORMÁTICO PARA ANÁLISIS GENÓMICO EN LA ASOCIACIÓN VACTERL")
+st.subheader('Bernat Moreno Batlle')
 
+# Adding a header to the sidebar
+st.sidebar.header("TFM - Bernat Moreno Batlle")
 
-def run():
-    st.set_page_config(
-        page_title="Hello",
-        page_icon="👋",
-    )
+# Defining a function for filtering and displaying the dataset "DATA_VCF.csv"
+def dataset_filtro_func():
+    # Caching the data to improve performance
+    @st.cache_data
+    def get_data():
+        # Displaying the title
+        st.write("#### Number of effects by impact and region:")
+        # Reading the dataset from a CSV file
+        dataset_filtro = pd.read_csv("./DATA_VCF.csv")
+        return dataset_filtro.set_index("#GeneName")
+    try:
+        # Calling the cached function to get the dataset
+        dataset_filtro = get_data()
+        # Setting the ENSEMBL column with a link
+        dataset_filtro["ENSEMBL"] = "https://www.ensembl.org/Homo_sapiens/Gene/Summary?db=core;g="+dataset_filtro["ENSEMBL"]
+        # Providing user interface elements for selecting filters
+        geneName = st.multiselect("Choose GeneName", list(dataset_filtro.index.unique()), placeholder="e.g. A2M")
+        bioType = st.multiselect("Choose BioType", list(dataset_filtro["BioType"].unique()), placeholder="e.g. rRNA")
+        high = list(dataset_filtro["HIGH"].unique())
+        low = list(dataset_filtro["LOW"].unique())
+        moderate = list(dataset_filtro["MODERATE"].unique())
+        modifier = list(dataset_filtro["MODIFIER"].unique())
+        # Styling for better UI presentation
+        st.markdown("""
+            <style>
+                div[data-testid="column"] {
+                    width: fit-content !important;
+                    flex: unset;
+                }
+                div[data-testid="column"] * {
+                    width: fit-content !important;
+                }
+            </style>
+            """, unsafe_allow_html=True)
+        # Creating columns for buttons to display horizontally
+        col1, col2, col3, col4 = st.columns([1,1,1,1])
+        # Adding the buttons to each column
+        with col1:
+            highButton = st.button("HIGH")
+        with col2:
+            lowButton = st.button("LOW")
+        with col3:
+            moderateButton = st.button("MODERATE")
+        with col4:
+            modifierButton = st.button("MODIFIER")
+        ifhigh = ["LOW", "MODERATE", "MODIFIER"]
+        iflow = ["HIGH", "MODERATE", "MODIFIER"]
+        ifmoderate = ["HIGH", "LOW", "MODIFIER"]
+        ifmodifier = ["HIGH", "LOW", "MODERATE"]
 
-    st.write("# Welcome to Streamlit! 👋")
-
-    st.sidebar.success("Select a demo above.")
-
-    st.markdown(
+        # Handling button clicks and filtering the data accordingly
+        if highButton:
+            data = dataset_filtro.loc[dataset_filtro["HIGH"].isin(high)]
+            if bioType and highButton:
+                bioData = dataset_filtro.loc[dataset_filtro["BioType"].isin(bioType)]
+                # Updating the list of columns to exclude impact on the DataFrame 
+                ifhigh = [col for col in bioData.columns if col not in ifhigh]
+                # Making the ENSEMBL column clickable
+                st.dataframe(
+                        bioData[ifhigh],
+                        column_config={
+                            "ENSEMBL": st.column_config.LinkColumn("ENSEMBL", display_text="^https://www\.ensembl\.org/Homo_sapiens/Gene/Summary\?db=core;g=(.*?)$")
+                        },
+                    )
+            else:
+                ifhigh = [col for col in data.columns if col not in ifhigh]
+                # Making the ENSEMBL column clickable
+                st.dataframe(
+                        data[ifhigh],
+                        column_config={
+                            "ENSEMBL": st.column_config.LinkColumn("ENSEMBL", display_text="^https://www\.ensembl\.org/Homo_sapiens/Gene/Summary\?db=core;g=(.*?)$")
+                        },
+                    )
+        elif lowButton:
+            data = dataset_filtro.loc[dataset_filtro["LOW"].isin(low)]
+            if bioType and lowButton:
+                bioData = dataset_filtro.loc[dataset_filtro["BioType"].isin(bioType)]
+                # Updating the list of columns to exclude impact on the DataFrame 
+                iflow = [col for col in bioData.columns if col not in iflow]
+                # Making the ENSEMBL column clickable
+                st.dataframe(
+                        bioData[iflow],
+                        column_config={
+                            "ENSEMBL": st.column_config.LinkColumn("ENSEMBL", display_text="^https://www\.ensembl\.org/Homo_sapiens/Gene/Summary\?db=core;g=(.*?)$")
+                        },
+                    )
+            else:
+                iflow = [col for col in data.columns if col not in iflow]
+                # Making the ENSEMBL column clickable
+                st.dataframe(
+                        data[iflow],
+                        column_config={
+                            "ENSEMBL": st.column_config.LinkColumn("ENSEMBL", display_text="^https://www\.ensembl\.org/Homo_sapiens/Gene/Summary\?db=core;g=(.*?)$")
+                        },
+                    )
+        elif moderateButton:
+            data = dataset_filtro.loc[dataset_filtro["MODERATE"].isin(moderate)]
+            if bioType and moderateButton:
+                bioData = dataset_filtro.loc[dataset_filtro["BioType"].isin(bioType)]
+                # Updating the list of columns to exclude impact on the DataFrame 
+                ifmoderate = [col for col in bioData.columns if col not in ifmoderate]
+                # Making the ENSEMBL column clickable
+                st.dataframe(
+                        bioData[ifmoderate],
+                        column_config={
+                            "ENSEMBL": st.column_config.LinkColumn("ENSEMBL", display_text="^https://www\.ensembl\.org/Homo_sapiens/Gene/Summary\?db=core;g=(.*?)$")
+                        },
+                    )
+            else:
+                ifmoderate = [col for col in data.columns if col not in ifmoderate]
+                # Making the ENSEMBL column clickable
+                st.dataframe(
+                        data[ifmoderate],
+                        column_config={
+                            "ENSEMBL": st.column_config.LinkColumn("ENSEMBL", display_text="^https://www\.ensembl\.org/Homo_sapiens/Gene/Summary\?db=core;g=(.*?)$")
+                        },
+                    )
+        elif modifierButton:
+            data = dataset_filtro.loc[dataset_filtro["MODIFIER"].isin(modifier)]
+            if bioType and modifierButton:
+                bioData = dataset_filtro.loc[dataset_filtro["BioType"].isin(bioType)]
+                # Updating the list of columns to exclude impact on the DataFrame 
+                ifmodifier = [col for col in bioData.columns if col not in ifmodifier]
+                # Making the ENSEMBL column clickable
+                st.dataframe(
+                        bioData[ifmodifier],
+                        column_config={
+                            "ENSEMBL": st.column_config.LinkColumn("ENSEMBL", display_text="^https://www\.ensembl\.org/Homo_sapiens/Gene/Summary\?db=core;g=(.*?)$")
+                        },
+                    )
+            else:
+                ifmodifier = [col for col in data.columns if col not in ifmodifier]
+                # Making the ENSEMBL column clickable
+                st.dataframe(
+                        data[ifmodifier],
+                        column_config={
+                            "ENSEMBL": st.column_config.LinkColumn("ENSEMBL", display_text="^https://www\.ensembl\.org/Homo_sapiens/Gene/Summary\?db=core;g=(.*?)$")
+                        },
+                    )
+        else:
+            if not geneName and not bioType:
+                # Making the ENSEMBL column clickable
+                st.dataframe(
+                    dataset_filtro,
+                    column_config={
+                    "ENSEMBL": st.column_config.LinkColumn("ENSEMBL", display_text="^https://www\.ensembl\.org/Homo_sapiens/Gene/Summary\?db=core;g=(.*?)$")
+                    },
+                )
+            elif bioType:
+                data = dataset_filtro.loc[dataset_filtro["BioType"].isin(bioType)]
+                # Making the ENSEMBL column clickable
+                st.dataframe(
+                    data,
+                    column_config={
+                    "ENSEMBL": st.column_config.LinkColumn("ENSEMBL", display_text="^https://www\.ensembl\.org/Homo_sapiens/Gene/Summary\?db=core;g=(.*?)$")
+                    },
+                )
+            else:
+                data = dataset_filtro.loc[geneName]
+                if bioType:
+                    st.write(data.loc[data["BioType"].isin(bioType)])
+                else:    
+                    # Making the ENSEMBL column clickable
+                    st.dataframe(
+                        data,
+                        column_config={
+                            "ENSEMBL": st.column_config.LinkColumn("ENSEMBL", display_text="^https://www\.ensembl\.org/Homo_sapiens/Gene/Summary\?db=core;g=(.*?)$")
+                        },
+                    )
+    except URLError as e:
+        # Displaying an error message if there is a URL connection error
+        st.error(
+            """
+            **This demo requires internet access.**
+            Connection error: %s
         """
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-        **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
-        ### Want to learn more?
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-        ### See more complex demos
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-    """
-    )
+            % e.reason
+        )
 
+# Defining a function for filtering and displaying the dataset "SNPEFF_VCF.csv"
+def vcf_filtro_func():
+    @st.cache_data
+    def get_data():
+        # Displaying the title
+        st.write("#### Variant Details (VCF file):")
+        # Reading the dataset from a CSV file
+        vcf_filtro = pd.read_csv("./SNPEFF_VCF.csv")
+        return vcf_filtro.set_index("#CHROM")
 
-if __name__ == "__main__":
-    run()
+    try:
+        # Calling the cached function to get the SNPEFF_VCF data
+        vcf_filtro = get_data()
+        # Setting the ENSEMBL column with a link
+        vcf_filtro["ENSEMBL"] = "https://www.ensembl.org/Homo_sapiens/Location/View?r="+vcf_filtro.index+":"+vcf_filtro["POS"].apply(str)
+        # Providing user interface elements for selecting filters
+        chrom = st.multiselect(
+            "Choose CHROM", list(vcf_filtro.index.unique()), placeholder="e.g. chr15"
+        )
+        # Handling CHROM selection and displaying the filtered data
+        if not chrom:
+            # Making the ENSEMBL column clickable
+            st.dataframe(
+                vcf_filtro,
+                column_config={
+                    "ENSEMBL": st.column_config.LinkColumn("ENSEMBL", display_text="Ubicación")
+                },
+            )
+        else:
+            data = vcf_filtro.loc[chrom]
+            # Making the ENSEMBL column clickable
+            st.dataframe(
+                data,
+                column_config={
+                    "ENSEMBL": st.column_config.LinkColumn("ENSEMBL", display_text="Ubicación")
+                },
+            )
+    except URLError as e:
+        # Displaying an error message if there is a URL connection error
+        st.error(
+            """
+            **This demo requires internet access.**
+            Connection error: %s
+        """
+            % e.reason
+        )
+
+# Calling the functions to display the filtered datasets
+dataset_filtro_func()
+vcf_filtro_func()
--- README.md
+++ README.md
@@ -1,7 +1,5 @@
-# Welcome to streamlit
+# DESIGN AND IMPLEMENTATION OF A BIOINFORMATICS PIPELINE FOR GENOMIC ANALYSIS AT THE VACTERL ASSOCIATION
 
-This is the app you get when you run `streamlit hello`, extracted as its own app.
+![image](https://github.com/polmoreno/test-streamlit-b/assets/62899431/6721f35e-ae8d-4eac-bd88-d35f9fce42aa)
+![image](https://github.com/polmoreno/test-streamlit-b/assets/62899431/e970a3a6-7ffa-49e1-852a-2bf63cddbbbc)
 
-Edit [Hello.py](./Hello.py) to customize this app to your heart's desire. ❤️
-
-Check it out on [Streamlit Community Cloud](https://st-hello-app.streamlit.app/)
